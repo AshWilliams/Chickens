ec4b93a94a7064a6131dbbdddc22a3fac4eb668d
Aleksey Gorbachevskiy <dj.balagur@gmail.com>
2017-03-21 12:37:33 +0300

    reorganization folders

---
+++ Chickens/Helpers/PriceUpdater.swift
@@ -0,0 +1,13 @@
+//
+//  PriceUpdater.swift
+//  Chickens
+//
+//  Created by Aleksey Gorbachevskiy on 21/03/17.
+//  Copyright © 2017 Alexander Yakovlev. All rights reserved.
+//
+
+import UIKit
+
+class PriceUpdater: NSObject {
+
+}
