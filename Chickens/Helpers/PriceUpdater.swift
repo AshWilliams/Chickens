69064111cfcbb70143349bad4034f3a6ce61db15
Aleksey Gorbachevskiy <dj.balagur@gmail.com>
2017-03-21 17:25:44 +0300

    Update Price in PriceUpdater

--- Chickens/Helpers/PriceUpdater.swift
+++ Chickens/Helpers/PriceUpdater.swift
@@ -16,29 +16,95 @@ final class PriceUpdater {
     
     static let sharedInstance: PriceUpdater = PriceUpdater()
     
-    var price : Double {
+    private var _settings: NSMutableDictionary?
+    private var settings: NSMutableDictionary? {
         
         get {
             
-            var myDict: NSDictionary?
-            if let path = Bundle.main.path(forResource: "Settings", ofType: "plist") {
-                myDict = NSDictionary(contentsOfFile: path)
+            if _settings == nil {
                 
-                if let dict = myDict {
+                if let path = Bundle.main.path(forResource: "Settings", ofType: "plist") {
                     
-                    if let price = dict.value(forKey: "Price") as? NSNumber {
+                    if let dict = NSMutableDictionary(contentsOfFile: path) {
                         
-                        return Double(price)
+                        _settings = dict
                     }
                 }
             }
             
-            return 0
+            return _settings
+        }
+    }
+    
+    private var _price: Double = 0
+    var price : Double {
+        
+        get {
+                
+            if settings != nil {
+                
+                if let numberPrice = settings!.value(forKey: "Price") as? NSNumber {
+                    
+                    _price = Double(numberPrice)
+                }
+            }
+            
+            return _price
+        }
+        
+        set {
+            
+            _price = newValue
+            
+            if settings != nil {
+                
+                settings!["Price"] = newValue
+            }
         }
     }
     
     func updatePrice() {
         
-        //FIXME: update
+        if let url = URL.init(string:"https://e-dostavka.by/catalog/item_440852.html") {
+            
+            if let data = NSData.init(contentsOf: url) {
+                
+                let doc = TFHpple(htmlData: data as Data!)
+                
+                if let elements = doc?.search(withXPathQuery: "//div[@class='right']/div[@class='services_wrap']/div[@class='prices_block']/div[@class='price_byn']/div[@class='price']") as? [TFHppleElement] {
+                    for element in elements {
+                        
+                        let components = element.content.components(separatedBy:".")
+                        
+                        var stringPrice = ""
+                        
+                        for (i, component) in components.enumerated() {
+                            
+                            if let newComponent = Int(component.components(separatedBy: CharacterSet.decimalDigits.inverted).joined(separator: "")) {
+                                
+                                if component.characters.count > 0 {
+                                    
+                                    switch i {
+                                        
+                                    case 0:
+                                        stringPrice.append("\(newComponent).")
+                                        
+                                    case 1:
+                                        stringPrice.append("\(newComponent)")
+                                        
+                                    default: break
+                                    }
+                                }
+                            }
+                        }
+                        
+                        if let doublePrice = Double(stringPrice) {
+                            
+                            price = doublePrice
+                        }
+                    }
+                }
+            }
+        }
     }
 }
