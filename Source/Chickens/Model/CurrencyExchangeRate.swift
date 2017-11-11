b32b48adbaccb1cc6755473f42956531ce0c8100
Sasha Kid <kidpost@gmail.com>
2017-11-11 10:10:01 +0300

    models are in progress

--- Source/Chickens/Model/CurrencyExchangeRate.swift
+++ Source/Chickens/Model/CurrencyExchangeRate.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+import SwiftyJSON
+import Money
+import Moya_SwiftyJSONMapper
+
+enum CurrencyType: String {
+  case BYN
+  case USD
+}
+/*
+struct CurrencyExchangeRate: ALSwiftyJSONAble {
+  let value: Decimal
+  let pair: (CurrencyType, CurrencyType)
+  
+  init?(jsonData: JSON) {
+    if jsonData["results"]["BYN_USD"]["val"] != JSON.null {
+      self.value = Decimal(jsonData["results"]["BYN_USD"]["val"].double!)
+    } else {
+      self.value = 0.0
+    }
+  }
+}
+
+extension CurrencyExchangeRate {
+  func stringFrom(_ pair: (CurrencyType, CurrencyType)) -> String? {
+    return "\(pair.0)_\(pair.1)"
+  }
+  
+  func pairFrom(_ string: String) -> (CurrencyType, CurrencyType)? {
+    let strings = string.components(separatedBy: "_")
+    var pair: (CurrencyType, CurrencyType)
+    guard let type0 = CurrencyType.init(rawValue: strings[0]), let type1 = CurrencyType.init(rawValue: strings[1]) else {
+      return nil
+    }
+    pair.0 = type0
+    pair.1 = type1
+    return pair
+  }
+}
+*/
